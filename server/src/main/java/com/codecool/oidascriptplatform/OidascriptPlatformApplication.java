1fc40ef77862296d4306b4d3d68082615bf7ea19
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-11-02 15:35:53 +0100

    Setup blank Java/Spring server

---
+++ server/src/main/java/com/codecool/oidascriptplatform/OidascriptPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.codecool.oidascriptplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidascriptPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OidascriptPlatformApplication.class, args);
+    }
+}
---
+++ server/src/test/java/com/codecool/oidascriptplatform/OidascriptPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.oidascriptplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidascriptPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
