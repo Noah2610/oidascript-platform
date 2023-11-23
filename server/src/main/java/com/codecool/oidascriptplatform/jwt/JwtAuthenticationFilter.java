efcb3f43f18185c640731a72a5a5c272d65b4c57
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-11-23 17:05:06 +0100

    server: Start rewriting auth stuff

--- server/src/main/java/com/codecool/oidascriptplatform/jwt/JwtAuthenticationFilter.java
+++ server/src/main/java/com/codecool/oidascriptplatform/jwt/JwtAuthenticationFilter.java
@@ -1,99 +1,50 @@
 package com.codecool.oidascriptplatform.jwt;
 
 import com.codecool.oidascriptplatform.AuthCookieManager;
-import com.codecool.oidascriptplatform.UserDetailsImpl;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.authentication.RememberMeServices;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.util.Optional;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 // TODO: Refactor authentication logic (create token, validate, store in context) into AuthenticationService?
 
 //@Component
-public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter /*UsernamePasswordAuthenticationFilter*/ {
-    private final AuthenticationManager authenticationManager;
-    private final JwtEncoder jwtEncoder;
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final AuthCookieManager cookieManager;
+    private final ProviderManager providerManager;
 
-    public JwtAuthenticationFilter(
-            RequestMatcher requestMatcher,
-            AuthenticationManager authenticationManager,
-            JwtEncoder jwtEncoder,
-            AuthCookieManager authCookieManager,
-            RememberMeServices rememberMe
-    ) {
-        super(requestMatcher, authenticationManager);
-        this.authenticationManager = authenticationManager;
-        this.jwtEncoder = jwtEncoder;
-        this.cookieManager = authCookieManager;
-        super.setRememberMeServices(rememberMe);
-
-        // TODO: do we need this?
-        // setFilterProcessesUrl("/sessions");
+    public JwtAuthenticationFilter(AuthCookieManager cookieManager, ProviderManager providerManager) {
+        super();
+        this.cookieManager = cookieManager;
+        this.providerManager = providerManager;
     }
 
     @Override
-    public Authentication attemptAuthentication(
-            HttpServletRequest req,
-            HttpServletResponse res
-    ) throws AuthenticationException {
-        UserDetails user;
-
-        try {
-            user = new ObjectMapper()
-                .readValue(req.getInputStream(), UserDetailsImpl.class);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        Optional<String> cookieOpt = cookieManager.getAuthCookie(request.getCookies());
+        if (cookieOpt.isEmpty()) {
+            return;
         }
 
-        return authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        user.getUsername(),
-                        user.getPassword(),
-                        user.getAuthorities()
-                )
-        );
+        String cookie = cookieOpt.get();
+
+        Authentication auth = new JwtTokenAuthentication(cookie);
+
+
+
+
     }
 
-    @Override
-    protected void successfulAuthentication(
-            HttpServletRequest req,
-            HttpServletResponse res,
-            FilterChain chain,
-            Authentication auth
-    ) throws IOException, ServletException {
-        System.out.println("------------------");
-        System.out.println(" AUTHENTICATOR success ");
-        System.out.println("------------------");
 
-//        String username = (String) auth.getPrincipal();
-//        String password = (String) auth.getCredentials();
-//        UserDetails user = new UserDetailsImpl(username, password);
-//        String token = jwtEncoder.encode(user);
-
-        // TODO: we might not need to set the cookie here, because the JwtRememberMeServices will do this
-        //res.addCookie(cookieManager.newAuthCookie(token));
-
-        //String body = user.getUsername() + " " + token;
-        //PrintWriter writer = res.getWriter();
-        //writer.write(body);
-        //writer.flush();
-
-        super.successfulAuthentication(req, res, chain, auth);
-        chain.doFilter(req, res);
-    }
 }
---
+++ server/src/main/java/com/codecool/oidascriptplatform/jwt/JwtTokenAuthentication.java
@@ -0,0 +1,50 @@
+package com.codecool.oidascriptplatform.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtTokenAuthentication implements Authentication {
+    // TODO: username
+    private final String token;
+
+    public JwtTokenAuthentication(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
